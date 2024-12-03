6a77d3dd077ed8fce37f0ccbb32916f98429a51f
Gabriel Santos <ga225360@gmail.com>
2024-12-03 12:36:13 -0300

    projeto final

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+   'Esportes',
+    'Qual foi o último time brasileiro a ganhar um mundial?',
+    'O Corinthians'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é nome da célula mais simples?',
+    'Procarionte'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a principal função dos cloroplastos?',
+    'Realizar a fotossíntese'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é renda fixa?',
+    'Renda fixa é um investimento no qual você recebe uma remuneração pré-estabelecida'
+)
